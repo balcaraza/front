6729be3cbde6acab3a3183150e2a58dc17b0afe5
Balam <55931922+balcaraza@users.noreply.github.com>
2023-07-26 11:20:50 -0600

    tabs navigation added

--- html/scripts.js
+++ html/scripts.js
@@ -16,3 +16,16 @@ function closeSidebar() {
     sidebarOpen = false;
   }
 }
+
+const targets = document.querySelectorAll('[data-target]')
+const content = document.querySelectorAll('[data-content]')
+
+targets.forEach(target => {
+	target.addEventListener('click', () => {
+		content.forEach(c => {
+			c.classList.remove('active')
+		})
+		const t = document.querySelector(target.dataset.target)
+		t.classList.add('active')
+	})
+})
